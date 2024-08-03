ccd67d6480ab8ef352fbac5d58c05221d9733581
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-08-04 01:42:44 +0900

    go2rtc 閉じないように

--- public.bat
+++ public.bat
@@ -1,11 +1,10 @@
 REM https://github.com/AlexxIT/go2rtc/
 REM winget install --id Cloudflare.cloudflared
 
-REM go2rtc起動
-start go2rtc.exe
-
 REM cloudflaredでローカルサーバーを公開
 start cloudflared tunnel --url localhost:1984
 
+REM go2rtc起動(別ウィンドウ)
+start cmd /k go2rtc.exe
 
 REM OBS : http://localhost:1984/api/webrtc?dst=livestream
