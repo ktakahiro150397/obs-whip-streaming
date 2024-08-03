20db226e71146b91c17e3eac777841b0c34890b3
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-08-03 13:40:20 +0900

    設定ファイル + 起動bat

---
+++ public.bat
@@ -0,0 +1,11 @@
+REM https://github.com/AlexxIT/go2rtc/
+REM winget install --id Cloudflare.cloudflared
+
+REM go2rtc起動
+start go2rtc.exe
+
+REM cloudflaredでローカルサーバーを公開
+cloudflared tunnel --url localhost:1984
+
+
+REM OBS : http://localhost:1984/api/webrtc?dst=livestream
